10f6897476fa7e07ad772a92a704de06c0377d9e
Raymond Raymundo <raymond.raymundo@fullspeedtechnologies.com>
2024-02-21 18:58:08 +0800

    CWC-Training: Created default layout.

---
+++ apps/blog-app/resources/views/layouts/common/header.blade.php
@@ -0,0 +1,60 @@
+<!-- header -->
+<header class="main-header">
+    <!-- Logo -->
+    <a href="#" class="logo">
+        <!-- mini logo for sidebar mini 50x50 pixels -->
+        <span class="logo-mini"><b>Blog</b></span>
+
+        <!-- logo for regular state and mobile devices -->
+        <span class="logo-lg"><b>Blog Management App</b></span>
+    </a>
+    <!-- Logo -->
+
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+        <!-- Sidebar toggle button-->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+        <!-- Sidebar toggle button-->
+
+        <!-- navbar-custom-menu -->
+        <div class="navbar-custom-menu">
+            <!-- nav navbar-nav -->
+            <ul class="nav navbar-nav">
+                <!-- User Account: style can be found in dropdown.less -->
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                        <img src="{{ asset('assets/images/no-avatar.jpg') }}" class="user-image" alt="alt-image">
+                        <span class="hidden-xs">Name</span>
+                    </a>
+
+                    <ul class="dropdown-menu">
+                        <!-- User image -->
+                        <li class="user-header">
+                            <img src="{{ asset('assets/images/no-avatar.jpg') }}" class="img-circle" alt="alt-image">
+                            <p>Name - Admin</p>
+                        </li>
+                        <!-- User image -->
+
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            <div class="pull-left">
+                                <a href="#" class="btn btn-default btn-flat">Edit Account</a>
+                            </div>
+                            <div class="pull-right">
+                                <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                            </div>
+                        </li>
+                        <!-- Menu Footer-->
+                    </ul>
+                </li>
+                <!-- User Account: style can be found in dropdown.less -->
+            </ul>
+            <!-- nav navbar-nav -->
+        </div>
+        <!-- navbar-custom-menu -->
+    </nav>
+    <!-- Header Navbar: style can be found in header.less -->
+</header>
+<!-- header -->
---
+++ apps/blog-app/resources/views/layouts/common/side_nav.blade.php
@@ -0,0 +1,54 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- Sidebar user panel -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="{{ asset('assets/images/no-avatar.jpg') }}" class="img-circle" alt="alt-image">
+            </div>
+            <div class="pull-left info">
+                <p>Name</p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Admin</a>
+            </div>
+        </div>
+        <!-- Sidebar user panel -->
+
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header">MAIN NAVIGATION</li>
+
+            <li>
+                <a href="#">
+                    <i class="fa fa-dashboard"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="#">
+                    <i class="fa fa-users"></i>
+                    <span>Users</span>
+                </a>
+            </li>
+
+
+            <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-newspaper-o"></i>
+                    <span>Articles</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Article Categories</a></li>
+                    <li><a href="#">Articles</a></li>
+                </ul>
+            </li>
+        </ul>
+        <!-- sidebar menu: : style can be found in sidebar.less -->
+    </section>
+    <!-- /.sidebar -->
+</aside>
+<!-- Left side column. contains the logo and sidebar -->
